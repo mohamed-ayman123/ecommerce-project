86b787ecd2dfcec38a3ada01048c5e938e673bcc
My World <abuxiong@Ammars-MacBook-Air.local>
2026-09-05 04:06:58 +0800

    Update user list

--- admin-dashboard/src/pages/users/UserList.jsx
+++ admin-dashboard/src/pages/users/UserList.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux'
-import { getAllUsers } from '../../Api/users'
+import { getAllUsers, deleteUsers } from '../../Api/users'
 import{
     setUsersLoading,
     setUsers,
@@ -15,8 +15,58 @@ const UserList = () => {
         (state) => state.users
     )
     useEffect (() => {
-      const fetchUsers = async () =>{
-
-     }
+        const fetchUsers = async () =>{
+        try {
+            dispatch ( setUsersLoading (true))
+            const data = await getAllUsers ()
+            dispatch(setUsers(data))
+        } catch (error) {
+            dispatch(setUsersError(error.message))
+        }
+    }
+    fetchUsers()
     }, [])
+    const handleDelete = async (id) => {
+        try {
+            await deleteUser(id)
+            const data = await getAllUsers()
+            dispatch(setUsers(data))
+        } catch (error) {
+            console.error (error)
+        }
+    }
+    if (isLoading) {
+        return <p>Loading users...</p>
+    }
+    if (error) {
+        return <p> {error} </p>
+    }
+    return (
+        <div>
+            <h1>Users</h1>
+            <p>Total users: {items.length}</p>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Role</th>
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {items.map((user) => {
+                        <tr key={user.id}>
+                            <td>{user.firstName} {user.lastName}</td>
+                            <td>{user.email}</td>
+                            <td>{user.role}</td>
+                            <td><button onClick= {() => handleDelete(user.id)}> Delete
+                                </button>
+                            </td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+        </div>
+    )
 }
