c4f1aa0b046472f63b8e1502e0c468263c658703
bashayer-BR <bishooo.sh.sh@gmail.com>
2026-09-06 19:45:46 +0300

    add navbar and some Edits

--- admin-dashboard/src/App.jsx
+++ admin-dashboard/src/App.jsx
@@ -1,8 +1,23 @@
-import Logo from '@/components/common/Logo'
-import Layout from './Components/Layout/Layout'
+import NavBar from "./Components/Layout/Navbar";
+import AppRoutes from "./routes/AppRoutes";
+import SideBar from "./Components/Layout/Sidebar"
 
 export default function App() {
   return (
-    <Layout/>
+    <div className="flex min-h-screen flex-col bg-[var(--color-bg-main)] md:flex-row dark:bg-[var(--color-dark-bg-main)]">
+      <div className="order-2 md:order-1">
+         <SideBar/>
+      </div>
+
+      <div className="order-1 flex min-w-0 flex-1 flex-col md:order-2">
+        
+        <NavBar/>
+
+        <main className="min-w-0 flex-1 p-4 md:p-8">
+          
+          <AppRoutes/>
+        </main>
+      </div>
+    </div>
   )
 }
